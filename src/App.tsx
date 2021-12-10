f91a2b5e21ef170b41bdb556dc24d191244b1541
IronLu233 <lrironsora@gmail.com>
2021-12-10 14:26:26 +0800

    init commit

---
+++ src/App.tsx
@@ -0,0 +1,25 @@
+import React, { FC, useEffect } from "react";
+import kleeURL from "./klee.jpg";
+
+const createMetaTag = (name: string, content: string) => {
+  const meta = document.createElement("meta");
+  meta.setAttribute("property", name);
+  meta.content = content;
+  return meta;
+};
+
+export const App: FC = () => {
+  useEffect(() => {
+    const metaTags: HTMLMetaElement[] = [];
+    metaTags.push(createMetaTag("og:title", "Klee"));
+    metaTags.push(createMetaTag("og:image", kleeURL));
+    metaTags.push(createMetaTag("og:description", "Fleeing Sunlight"));
+    metaTags.push(createMetaTag("og:url", window.location.href));
+    document.head.append(...metaTags);
+  }, []);
+  return (
+    <div>
+      <img src={kleeURL} alt="klee" />
+    </div>
+  );
+};
